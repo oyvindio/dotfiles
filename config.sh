bfc9d95708051bf1a4295aaad7b7d3a0c0c7672d
Øyvind Øvergaard <oyvind.overgaard@gmail.com>
2009-08-05 03:50:55 +0200

    link.sh should work now

---
+++ config.sh
@@ -0,0 +1,13 @@
+# the dotfiles we want to link
+DOTFILES=(
+    .bashrc.oyvindio
+    .emacs.d
+    .gitconfig
+    .gvimrc
+    .screenrc
+    .toprc
+    .vimrc
+    .vim
+    .Xdefaults
+    .xmodmap.swap-caps-ctrl
+)
--- link.sh
+++ link.sh
@@ -1,52 +1,41 @@
 #!/bin/bash
 
-WD=$(dirname "$0")
-cd "$WD"
+################################################################################
+# Symlinks config files specified in config.sh to ~/                           #
+################################################################################
 
-ACTION="$1"
+. config.sh
+
 ACTION_CLEAN="clean"
-
-DOTFILES=$(find . -maxdepth 1 -name ".*" ! -name ".git")
+ACTION="$1"
+SRC="$(pwd)/$(dirname $0)"
 
 # remove a link, if it exists
 function rm_file() {
-    file="$WD/$dotfile"
-
-    if [ -h "$1" ]
+    if [ -h "~/$1" ]
     then
-        rm -v "$1"
+        rm -v "~/$1"
     fi    
 }
 
+# link a file, unless it exists
 function link_file() {
-    file="$WD/$dotfile"
+    file="$SRC/$dotfile"
 
-    if [ -h "$1" ]
+    if [ -h "~/$1" ]
     then
-        echo "$1 is already linked!"
-    fi
-        
-    dst="/tmp/dotfiles"
-    if [ ! -e "$1" ]
-    then
-        ln -s "$file" "$dst/$1"
+        echo "~/$1 is already linked!"
     else
-        echo "$dst/$1 already exists, remove it first!"
+        if [ ! -e "~/$1" ]
+        then
+            ln -sv "$file" "~/"
+        else
+            echo "~/$1 already exists, remove it first!"
+        fi
     fi
 }
 
-function create_editor_backup_dirs() {
-    dst="/tmp/dotfiles"
-    for dir in "$dst/.vim-backup $dst/.emacs-backup"
-    do
-        if [ ! -d "$dir" ]
-        then
-            mkdir -v "$dir"
-        fi
-    done
-}
-
-for dotfile in "$DOTFILES"
+for dotfile in "${DOTFILES[@]}"
 do
     case "$ACTION" in
         "$ACTION_CLEAN")
@@ -57,5 +46,3 @@ do
         ;;
     esac
 done
-
-create_editor_backup_dirs
