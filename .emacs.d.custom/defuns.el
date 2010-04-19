6cee6c5160b41747397590808d12272f2f438602
Øyvind Øvergaard <oyvind.overgaard@gmail.com>
2010-04-19 11:07:42 +0200

    move bindings and defuns into separate files

---
+++ .emacs.d.custom/defuns.el
@@ -0,0 +1,26 @@
+;; source: http://steve.yegge.googlepages.com/my-dot-emacs-file
+(defun rename-file-and-buffer (new-name)
+  "Renames both current buffer and file it's visiting to NEW-NAME."
+  (interactive "sNew name: ")
+  (let ((name (buffer-name))
+    (filename (buffer-file-name)))
+    (if (not filename)
+    (message "Buffer '%s' is not visiting a file!" name)
+      (if (get-buffer new-name)
+      (message "A buffer named '%s' already exists!" new-name)
+    (progn
+      (rename-file name new-name 1)
+      (rename-buffer new-name)
+      (set-visited-file-name new-name)
+      (set-buffer-modified-p nil))))))
+
+;; source: http://github.com/defunkt/emacs/blob/master/defunkt/defuns.el
+(defun gist-buffer-confirm (&optional private)
+  (interactive "P")
+  (when (yes-or-no-p "Are you sure you want to Gist this buffer? ")
+    (gist-region-or-buffer private)))
+
+(defun diw-word-count ()
+  "Count words in buffer, excluding (X)HTML markup"
+  (interactive)
+  (shell-command-on-region (point-min) (point-max) "diw-wc.py"))
--- .emacs.d.custom/oyvindio.el
+++ .emacs.d.custom/oyvindio.el
@@ -17,7 +17,10 @@
 (setq default-tab-width 4)
 
 ;; set color theme
-(color-theme-railscasts)
+(eval-after-load "color-theme"
+  '(progn (color-theme-railscasts))
+  )
+
 ;;(color-theme-vivid-chalk)
 
 ;; highlight the current line
@@ -36,63 +39,10 @@
 ;; ANSI colors for M-x shell
 ;(autoload 'ansi-color-for-comint-mode-on "ansi-color" nil t)
 (add-hook 'shell-mode-hook 'ansi-color-for-comint-mode-on)
-
-;; keybinds
-;------------------------------------------------------------------------------
-(global-set-key (kbd "C-x c") 'comment-or-uncomment-region)
-(global-set-key  [C-tab] 'other-window)
-(global-set-key (kbd "C-r") 'replace-string)
-(global-set-key (kbd "C-c C-f") 'fill-region)
-(global-set-key (kbd "C-c C-g") 'gist-buffer-confirm)
-(global-set-key (kbd "C-x m") 'shell) ; rebind starter-kit eshell->shell
-
-;; defuns
-;------------------------------------------------------------------------------
-;; source: http://steve.yegge.googlepages.com/my-dot-emacs-file
-(defun rename-file-and-buffer (new-name)
-  "Renames both current buffer and file it's visiting to NEW-NAME."
-  (interactive "sNew name: ")
-  (let ((name (buffer-name))
-    (filename (buffer-file-name)))
-    (if (not filename)
-    (message "Buffer '%s' is not visiting a file!" name)
-      (if (get-buffer new-name)
-      (message "A buffer named '%s' already exists!" new-name)
-    (progn
-      (rename-file name new-name 1)
-      (rename-buffer new-name)
-      (set-visited-file-name new-name)
-      (set-buffer-modified-p nil))))))
-
-;; source: http://www.emacswiki.org/emacs/InsertAnyDate
-(require 'calendar)
-(defun insdate-insert-any-date (&optional days)
-  (interactive "p*")
-  (insert
-   (calendar-date-string
-    (calendar-gregorian-from-absolute
-     (+ (calendar-absolute-from-gregorian (calendar-current-date))
-        days)))))
-
-;; source: http://www.emacswiki.org/emacs/InsertingTodaysDate
-(require 'calendar)
-(defun insdate-insert-current-date (&optional omit-day-of-week-p)
-  "Insert today's date using the current locale.
-  With a prefix argument, the date is inserted without the day of
-  the week."
-  (interactive "P*")
-  (insert (calendar-date-string (calendar-current-date) nil
-                                omit-day-of-week-p)))
-
-;; source: http://github.com/defunkt/emacs/blob/master/defunkt/defuns.el
-(defun gist-buffer-confirm (&optional private)
-  (interactive "P")
-  (when (yes-or-no-p "Are you sure you want to Gist this buffer? ")
-    (gist-region-or-buffer private)))
-
-
-(defun diw-word-count ()
-  "Count words in buffer, excluding (X)HTML markup"
-  (interactive)
-  (shell-command-on-region (point-min) (point-max) "diw-wc.py"))
-
+;; Bindings for navigating shell command history
+(eval-after-load 'shell
+  '(progn
+     (define-key shell-mode-map [up] 'comint-previous-input)
+     (define-key shell-mode-map [down] 'comint-next-input)
+     (define-key shell-mode-map "\C-p" 'comint-previous-input)
+     (define-key shell-mode-map "\C-n" 'comint-next-input)))
