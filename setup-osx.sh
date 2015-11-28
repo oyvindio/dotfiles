aeca278113bec4a45efc6bbb9e80dad633adcbcb
Øyvind Ingebrigtsen Øvergaard <oyvind.overgaard@gmail.com>
2015-11-28 15:34:24 +0100

    Ensure $HOME/Library/Keyboard Layouts exists

--- setup-osx.sh
+++ setup-osx.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 cd "$(dirname $0)"
+mkdir -p "$HOME/Library/KeyBindings/"
 cp 'DefaultKeyBinding.dict' "$HOME/Library/KeyBindings/"
 cp 'U.S. International - Modified.keylayout' "$HOME/Library/Keyboard Layouts"
