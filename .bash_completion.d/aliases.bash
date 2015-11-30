1e4282df7b9af78fae57d85645986e51d2b90908
Øyvind Øvergaard <oyvind.overgaard@gmail.com>
2015-11-30 22:37:11 +0100

    Silence erroneous shellcheck warnings

--- .bash_completion.d/aliases.bash
+++ .bash_completion.d/aliases.bash
@@ -9,23 +9,26 @@ function alias_completion {
 
     # create array of function completion triggers, keeping multi-word triggers together
     eval "local completions=($(complete -p | sed -Ene "/$compl_regex/s//'\3'/p"))"
+    # shellcheck disable=SC2154
     (( ${#completions[@]} == 0 )) && return 0
 
     # create temporary file for wrapper functions and completions
+    # shellcheck disable=SC1012
     \rm -f "/tmp/${namespace}-*.tmp" # preliminary cleanup
     local tmp_file
     tmp_file="$(mktemp "/tmp/${namespace}-${RANDOM}.tmp")" || return 1
 
     # read in "<alias> '<aliased command>' '<command args>'" lines from defined aliases
-    local line; while read line; do
-        eval "local alias_tokens=($line)" 2>/dev/null || continue # some alias arg patterns cause an eval parse error 
+    local line
+    while read line; do
+        eval "local alias_tokens=($line)" 2>/dev/null || continue # some alias arg patterns cause an eval parse error
+        # shellcheck disable=SC2154
         local alias_name="${alias_tokens[0]}" alias_cmd="${alias_tokens[1]}" alias_args="${alias_tokens[2]# }"
 
         # skip aliases to pipes, boolan control structures and other command lists
         # (leveraging that eval errs out if $alias_args contains unquoted shell metacharacters)
         eval "local alias_arg_words=($alias_args)" 2>/dev/null || continue
 
-        echo "${completions[*]}"
         # skip alias if there is no completion function triggered by the aliased command
         [[ " ${completions[*]} " = *" $alias_cmd "* ]] || continue
         local new_completion
@@ -37,12 +40,13 @@ function alias_completion {
             # avoid recursive call loops by ignoring our own functions
             if [[ "${compl_func#_$namespace::}" == "$compl_func" ]]; then
                 local compl_wrapper="_${namespace}::${alias_name}"
-                    echo "function $compl_wrapper {
+                # shellcheck disable=SC2154
+                echo "function $compl_wrapper {
                         (( COMP_CWORD += ${#alias_arg_words[@]} ))
                         COMP_WORDS=($alias_cmd $alias_args \${COMP_WORDS[@]:1})
                         $compl_func
                     }" >> "$tmp_file"
-                    new_completion="${new_completion/ -F $compl_func / -F $compl_wrapper }"
+                new_completion="${new_completion/ -F $compl_func / -F $compl_wrapper }"
             fi
         fi
 
@@ -50,5 +54,6 @@ function alias_completion {
         new_completion="${new_completion% *} $alias_name"
         echo "$new_completion" >> "$tmp_file"
     done < <(alias -p | sed -Ene "s/$alias_regex/\1 '\2' '\3'/p")
+    # shellcheck disable=SC1012
     source "$tmp_file" && \rm -f "$tmp_file"
 }; alias_completion
