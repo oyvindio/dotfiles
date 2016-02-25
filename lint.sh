544254540eb631d6bbfd56cdd80669cbfe698651
Øyvind Ingebrigtsen Øvergaard <oyvind.overgaard@gmail.com>
2016-02-25 14:59:04 +0100

    Fix lint when running from other directory

--- lint.sh
+++ lint.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
+cd "$(dirname "$0")" || exit 1
 FILES=($(find .bash.d .bash_completion.d .bashrc .bash_profile .bash_completion lint.sh setup-osx.sh -type f))
 shellcheck --shell=bash --format=gcc "${FILES[@]}"
