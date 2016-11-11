5ebc85c776e9ea23828c09987c3aba06d2fb6d54
Øyvind Ingebrigtsen Øvergaard <oyvind.overgaard@gmail.com>
2016-11-11 18:31:35 +0100

    Use official shellcheck docker image when running lint in travis

--- lint.sh
+++ lint.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
 cd "$(dirname "$0")" || exit 1
+if [[ "$TRAVIS" == "travis" ]]; then
+    alias shellcheck='docker run koalaman/shellcheck:latest'
+fi
 FILES=($(find .bash.d .bash_completion.d .bashrc .bash_profile .bash_completion lint.sh setup-osx.sh -type f))
 shellcheck --shell=bash --format=gcc "${FILES[@]}"
