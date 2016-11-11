85f0468ff3891677790b01a49d0ed9f55c4e9045
Øyvind Ingebrigtsen Øvergaard <oyvind.overgaard@gmail.com>
2016-11-11 19:18:28 +0100

    Can't use alias in non-interactive shell

--- lint.sh
+++ lint.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 cd "$(dirname "$0")" || exit 1
-if [[ "$TRAVIS" == "travis" ]]; then
-    alias shellcheck='docker run koalaman/shellcheck:latest'
+if [[ "$TRAVIS" == "true" ]]; then
+    SHELLCHECK="docker run --rm -it -v $(pwd):/mnt koalaman/shellcheck:latest"
+else
+    SHELLCHECK='shellcheck'
 fi
 FILES=($(find .bash.d .bash_completion.d .bashrc .bash_profile .bash_completion lint.sh setup-osx.sh -type f))
-shellcheck --shell=bash --format=gcc "${FILES[@]}"
+$SHELLCHECK --shell=bash --format=gcc "${FILES[@]}"
