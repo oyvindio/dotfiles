8693bc17ae8434b70c23f3d92074e33398b32e8f
Øyvind Ingebrigtsen Øvergaard <oyvind.overgaard@gmail.com>
2024-11-21 13:22:08 +0100

    Remove custom binding for C-d, it messes with delete-char behavior

--- .config/fish/functions/fish_user_key_bindings.fish
+++ .config/fish/functions/fish_user_key_bindings.fish
@@ -1,8 +1,4 @@
 function fish_user_key_bindings
-    # ignore one EOF/C-d, similar to IGNOREEOF=1 in bash
-    bind \cd delete-char
-    bind \cd\cd delete-or-exit
-
     # fun emacs bindings
     bind \cx\cf 'emacsclient --no-wait --eval "(select-frame-set-input-focus (selected-frame))" "(counsel-projectile-find-file)"; commandline -f repaint'
     bind \cxg 'emacsclient --no-wait --eval "(select-frame-set-input-focus (selected-frame))" "(magit-status)"; commandline -f repaint'
